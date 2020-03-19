750fe0b0f93ecf1710e0dbb3b6adf0c5594e349a
Martin Monteagudo <amarti77@t-systems.es>
2020-03-19 09:29:51 +0100

    sd

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
